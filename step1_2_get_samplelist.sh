089da670cb391aa7cb8274da91bfe96568115489
TanasijeRUnibe <tanasije.rakic@students.unibe.ch>
2025-01-31 19:25:02 +0100

    Update step1_2_get_samplelist.sh

--- step1_2_get_samplelist.sh
+++ step1_2_get_samplelist.sh
@@ -8,11 +8,16 @@
 #SBATCH --error=array_%J.err
 #SBATCH --partition=pibu_el8
 
+# Directory containing FASTQ files (provided as an argument when running the script)
 FASTQ_FOLDER=$1
 
+# Loop through all FASTQ files that end with "_1.fastq.gz"
 for FILE in $FASTQ_FOLDER/*_*1.fastq.gz
 do 
+    # Extract the sample prefix (remove _1.fastq.gz or _2.fastq.gz)
     PREFIX="${FILE%_*.fastq.gz}"
-    SAMPLE=`basename $PREFIX`
-    echo -e "${SAMPLE}\t$FILE\t${FILE%?.fastq.gz}2.fastq.gz" 
-done
+    SAMPLE=`basename $PREFIX`  # Get only the sample name
+
+    # Print the sample name, Read 1 file, and Read 2 file in tab-separated format
+    echo -e "${SAMPLE}\t$FILE\t${FILE%?_1.fastq.gz}_2.fastq.gz" 
+done > samplelist.tsv  # Save the output to samplelist.tsv
